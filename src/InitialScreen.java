5e91e314811c8b09773e502c7d6d4edac3989058
ssohye <ssohye@icloud.com>
2022-10-27 15:05:16 +0900

    z

---
+++ src/InitialScreen.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class InitialScreen extends JFrame {
+    public InitialScreen(){
+        setSize(1125,2436);
+        setTitle("Instagram");
+        setVisible(true);
+    }
+}
