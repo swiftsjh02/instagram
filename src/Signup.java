2428081201c4150d86bc24a2d35ec33492c5067c
kimuijin <whitekuj02@naver.com>
2022-11-04 16:04:52 +0900

    회원가입 form

--- src/Signup.java
+++ src/Signup.java
@@ -24,7 +24,7 @@ public class Signup extends JFrame {
                 database con=new database();
 
                 //database 안에 회원가입 메서드를 불러와야함
-                //con.register(id,password);
+                con.register(id,password);
             }
         });
     }
