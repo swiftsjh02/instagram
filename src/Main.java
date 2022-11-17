6a57467b7e1b24e5a0f04be9bc805a6745d19346
taewan <taewan2002@gachon.ac.kr>
2022-11-18 02:44:27 +0900

    story페이지 형식 제작

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         // Login mylogin = new Login();
         // mainFeed a = new mainFeed();
-
+        story a = new story();
     }
 }
