0cdebc934b462a184cbf52e8423d91d2d4b642fc
kimuijin <whitekuj02@naver.com>
2022-11-18 01:33:45 +0900

    회원가입 form

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        Login mylogin = new Login();
-        // mainFeed a = new mainFeed();
+        // Login mylogin = new Login();
+        mainFeed a = new mainFeed();
     }
 }
