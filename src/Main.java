9de0c37da742b7e8dd10f77a162ec70f513ce32d
taewan <taewan2002@gachon.ac.kr>
2022-11-18 01:58:40 +0900

    이미지파일 추가

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         // Login mylogin = new Login();
-        mainFeed a = new mainFeed();
+        // mainFeed a = new mainFeed();
+
     }
 }
