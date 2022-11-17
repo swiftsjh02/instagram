941e71fe169141cebac4d23b1ec25c41ab6cb9dc
kimuijin <whitekuj02@naver.com>
2022-11-18 01:33:45 +0900

    회원가입 form

--- src/mainFeed.java
+++ src/mainFeed.java
@@ -3,18 +3,15 @@ import java.awt.*;
 import java.beans.Visibility;
 
 public class mainFeed extends JFrame{
+
     private JPanel panelBottom;
     private JButton homeButton;
     private JButton userHomeButton;
     private JButton searchButton;
     private JButton reelsButton;
     private JButton shopButton;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JTextPane TextPane;
     private JPanel main;
+    private JLabel icon;
 
     public mainFeed(){
         ImgSetSize home = new ImgSetSize("src/IMG/home.png", 50, 50);
@@ -31,6 +28,9 @@ public class mainFeed extends JFrame{
 
 //        ImgSetSize userhome = new ImgSetSize("src/IMG/.png", 50, 50);
 //        userHomeButton.setIcon(home.getImg());
+
+        ImgSetSize mainphoto = new ImgSetSize("src/IMG/login.png", 100, 50);
+        icon.setIcon(mainphoto.getImg());
 
         setContentPane(main);
         setSize(600,1100);
