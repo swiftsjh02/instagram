b698da6c8abb00b0a7d655f6f799edd744846a63
taewan <taewan2002@gachon.ac.kr>
2022-11-18 01:51:17 +0900

    이미지파일 추가

---
+++ src/story.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class story {
+    private JTextArea 메시지보내기TextArea;
+    private JTextArea id;
+    private JPanel like;
+    private JPanel dm;
+    private JPanel profile;
+    private JPanel option;
+    private JPanel exit;
+}
