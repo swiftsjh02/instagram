27c52e4e969d11d21ffc43c628d1f824b987a3db
yejiikim <yejikim001120@gmail.com>
2022-11-24 19:21:29 +0900

    user feed, reels, search 1차

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Login mylogin = new Login();
         //mainFeed a = new mainFeed();
+
     }
 }
--- src/reels.java
+++ src/reels.java
@@ -1,2 +1,14 @@
+import javax.swing.*;
+
 public class reels {
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton likeButton;
+    private JButton commentsButton;
+    private JButton moreButton;
+    private JButton DMButton;
 }
