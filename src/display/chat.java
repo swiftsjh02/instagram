db8678df24e99971fe81adb83bbc3578728752dd
kimuijin <whitekuj02@naver.com>
2022-11-25 20:55:56 +0900

    scroll 했어요 드디어

--- src/display/chat.java
+++ src/display/chat.java
@@ -2,6 +2,14 @@ package display;
 
 import javax.swing.*;
 
-public class chat {
+public class chat extends JFrame{
     private JPanel main;
+    private JPanel message;
+    private JTextField textField1;
+    private JButton send;
+    private JPanel message_user;
+
+    public chat(){
+
+    }
 }
