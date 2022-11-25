0f81f045bd7ccc2c053b817768cdbf6dadd47673
yejiikim <yejikim001120@gmail.com>
2022-11-25 21:36:19 +0900

    search Feed 제외 나머지

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
 import javax.swing.*;
 import display.*;
-import chatting.*;
-import function.*;
-
-import java.awt.event.FocusEvent;
-
 public class Main {
     public static void main(String[] args) {
-        Login mylogin = new Login();
-        // Signup mysignup = new Signup();
+        //Login mylogin = new Login();
         //mainFeed a = new mainFeed();
-        // FocusEventTest a = new FocusEventTest();
-
+        //userFeed user = new userFeed();
+        //reels myReels = new reels();
+        shop myShop = new shop();
     }
 }
--- src/display/reels.java
+++ src/display/reels.java
@@ -3,15 +3,59 @@ package display;
 import javax.swing.*;
 import function.*;
 
-public class reels {
-    private JPanel panel1;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
+public class reels extends JFrame{
+    private JPanel main;
+    private JButton userhomeButton;
+    private JButton homeButton;
+    private JButton userHomeButton;
+    private JButton shopButton;
+    private JButton reelsButton;
+    private JButton searchButton;
+    private JLabel camera;
+    private JButton imageButton;
     private JButton likeButton;
-    private JButton commentsButton;
     private JButton moreButton;
-    private JButton DMButton;
+    private JButton dmButton;
+    private JButton commentsButton;
+    private JButton 팔로우Button;
+    private JPanel bottom;
+
+    public reels(){
+        ImgSetSize home = new ImgSetSize("src/IMG/home.png", 50, 50);
+        homeButton.setIcon(home.getImg());
+
+        ImgSetSize search = new ImgSetSize("src/IMG/search.png", 50, 50);
+        searchButton.setIcon(search.getImg());
+
+        ImgSetSize reels = new ImgSetSize("src/IMG/reels.png", 50, 50);
+        reelsButton.setIcon(reels.getImg());
+
+        ImgSetSize shop = new ImgSetSize("src/IMG/shop.png", 50, 50);
+        shopButton.setIcon(shop.getImg());
+
+        ImgSetSize userHome = new ImgSetSize("src/IMG/userhomefeed.png", 50, 50);
+        userhomeButton.setIcon(userHome.getImg());
+
+        ImgSetSize camera1 = new ImgSetSize("src/IMG/camera1.png", 50,50);
+        camera.setIcon(camera1.getImg());
+
+        ImgSetSize like = new ImgSetSize("src/IMG/like.png", 50,50);
+        likeButton.setIcon(like.getImg());
+
+        ImgSetSize comments = new ImgSetSize("src/IMG/comments.png", 50,50);
+        commentsButton.setIcon(comments.getImg());
+
+        ImgSetSize dm = new ImgSetSize("src/IMG/dm.png", 50,50);
+        dmButton.setIcon(dm.getImg());
+
+        ImgSetSize more = new ImgSetSize("src/IMG/more.png", 50,50);
+        moreButton.setIcon(more.getImg());
+
+
+        setContentPane(main);
+        setSize(850,1000);
+        bottom.setSize(850,50);
+        setBounds(0,0,850,1000);
+        setVisible(true);
+    }
 }
