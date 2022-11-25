7dba06c1275352f5be087ee71e732bfa41b1dd69
kimuijin <whitekuj02@naver.com>
2022-11-25 21:19:15 +0900

    scroll 했어요 드디어

--- src/display/dm.java
+++ src/display/dm.java
@@ -1,10 +1,13 @@
 package display;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class dm extends JFrame{
+    private final static ArrayList<String> room_id = new ArrayList<String>();
     private JPanel main;
     private JButton createRoom;
     private JScrollPane roomPanel;
@@ -12,13 +15,35 @@ public class dm extends JFrame{
 
     public dm(){
         // 방 목록 업데이트
-        // client에서 방목록을 불러오기
+        // client에서 방목록을 불러오기 room_id
+
         setContentPane(main);
 
         setSize(850, 1000);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(0,0,850,1000);
         //
+        roomPanel.getVerticalScrollBar().setUnitIncrement(15);
+
+        GridBagLayout Gbag = new GridBagLayout();
+        room.setLayout(Gbag);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        for(int i = 0;i<room_id.size();i++){
+            roomPanel pane = new roomPanel(room_id.get(i));
+            gbc.fill = GridBagConstraints.BOTH;
+            gbc.ipadx = 850;
+            gbc.ipady = 100;
+            gbc.gridx = 0;
+            gbc.gridy = i;
+            Gbag.setConstraints(pane,gbc);
+            room.add(pane);
+            room.updateUI();
+        }
+        roomPanel.setViewportView(room);
+        roomPanel.setVisible(true);
+        room.setVisible(true);
         createRoom.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -40,6 +65,7 @@ public class dm extends JFrame{
             this.room_id = id;
 
             this.in = new JButton();
+
 
 
 
