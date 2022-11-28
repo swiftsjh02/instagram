bd222dc6720292173637f8da3fda727b0f398968
“ssohye” <ssohye@icloud.com>
2022-11-29 02:15:50 +0900

    일단 오늘 7시 밥 먹기 전 과정

--- src/chatting/directorychoose.java
+++ src/chatting/directorychoose.java
@@ -14,7 +14,7 @@ public class directorychoose {
         JFileChooser chooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory()); // 디렉토리 설정
         chooser.setCurrentDirectory(new File("/")); // 현재 사용 디렉토리를 지정
         chooser.setAcceptAllFileFilterUsed(true);   // Fileter 모든 파일 적용
-        chooser.setDialogTitle("파일을 선택해주세요"); // 창의 제목
+        chooser.setDialogTitle("폴더를 선택해주세요"); // 창의 제목
         chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY); // 파일 선택 모드
 
         FileNameExtensionFilter filter = new FileNameExtensionFilter("Binary File", "cd11"); // filter 확장자 추가
