5170f6f2550800850c1e829252496679745899c4
yejiikim <yejikim001120@gmail.com>
2022-11-28 19:18:11 +0900

    search post 수정

--- src/display/search.java
+++ src/display/search.java
@@ -1,8 +1,10 @@
 package display;
 import function.ImgSetSize;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class search extends JFrame {
     private JButton homeButton;
@@ -14,6 +16,9 @@ public class search extends JFrame {
     private JPanel main;
     private JPanel bottom;
     private JPanel top;
+    private JScrollPane searchPost_scroll;
+    private JPanel scroll;
+    private JPanel searchPost;
 
     public search(int session,String user_id) {
         ImgSetSize home = new ImgSetSize("src/IMG/home.png", 50, 50);
@@ -31,7 +36,55 @@ public class search extends JFrame {
         ImgSetSize userFeed = new ImgSetSize("src/IMG/userhomefeed.png", 50, 50);
         userhomeButton.setIcon(home.getImg());
 
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        ArrayList<Integer> post_list = new ArrayList<Integer>();
+        // get_post_list();
+        post_list.add(1);
+        post_list.add(2);
+        post_list.add(3);
+        post_list.add(4);
+        post_list.add(5);
+        post_list.add(6);
+        post_list.add(7);
+        post_list.add(8);
+        post_list.add(9);
+        post_list.add(10);
+        post_list.add(11);
+        post_list.add(12);
+        post_list.add(13);
+        post_list.add(14);
+        post_list.add(15);
+
+
+        searchPost_scroll.getVerticalScrollBar().setUnitIncrement(15);
+
+        GridBagLayout Gbag = new GridBagLayout();
+        scroll.setLayout(Gbag);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        for(int i = 0;i<post_list.size();i++){
+            search_post pane = new search_post();
+            gbc.fill = GridBagConstraints.BOTH;
+            gbc.ipadx = 200;
+            gbc.ipady = 200;
+            gbc.gridx = i%3;
+            gbc.gridy = i/3;
+            Gbag.setConstraints(pane,gbc);
+            scroll.add(pane);
+            scroll.updateUI();
+        }
+        searchPost_scroll.setViewportView(scroll);
+        searchPost_scroll.setVisible(true);
+        scroll.setVisible(true);
+
+
+        setContentPane(main);
+        setSize(850,1000);
+        bottom.setSize(850,50);
+        setBounds(0,0,850,1000);
+        setVisible(true);
         setContentPane(main);
         setSize(850, 1000);
         bottom.setSize(850, 50);
@@ -86,6 +139,25 @@ public class search extends JFrame {
                 a.setVisible(true);
             }
         });
+    }
+    public class search_post extends JPanel{
+        private JButton searchPost_num;
+
+        search_post(){
+            setLayout(new GridLayout(1,1));
+            setSize(200,200);
+            searchPost_num = new JButton();
+            searchPost_num.setSize(200,200);
+            add(searchPost_num);
+
+
+            searchPost_num.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                }
+            });
+        }
     }
 
 }
