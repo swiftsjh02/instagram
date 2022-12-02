b25a65263969d8517d602e4e4e3f2c2164a48da7
yejiikim <yejikim001120@gmail.com>
2022-12-02 19:47:22 +0900

    add feed 수정

---
+++ src/display/addFeed.java
@@ -0,0 +1,19 @@
+package display;
+
+import javax.swing.*;
+
+public class addFeed {
+    private JButton share;
+    private JButton cancel;
+    private JLabel newPost;
+    private JLabel tag;
+    private JLabel music;
+    private JLabel location;
+    private JLabel other;
+    private JLabel facebook;
+    private JLabel twitter;
+    private JLabel tumblr;
+    private JLabel settings;
+    private JTextField 문구입력TextField;
+    private JPanel main;
+}
