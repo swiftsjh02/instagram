3068b76de4a026ac31de4206dea0dc9b3595ec08
taewan2002 <taewan2002@naver.com>
2022-12-03 22:32:09 +0900

    get_data();

--- src/display/userFeed.java
+++ src/display/userFeed.java
@@ -4,6 +4,7 @@ import chatting.ListeningThread;
 import chatting.chatting_client;
 import function.ImgSetSize;
 import function.follow;
+import function.get_data;
 import function.unfollow;
 
 import javax.swing.*;
@@ -136,15 +137,18 @@ public class userFeed extends JFrame{
         client.get_post_num(10, user_id);
         client.get_following_num(19, user_id);// 팔로잉
         client.get_follow_num(20, user_id); //팔로워
-        try {
-            TimeUnit.MILLISECONDS.sleep(500);
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-        postNum.setText(String.valueOf(t1.getPost_num()));
-        followerNum.setText(String.valueOf(t1.get_follow_num()));
-        followingNum.setText(String.valueOf(t1.get_follower_num()));
+        get_data getData = new get_data();
+        getData.setType10(10, user_id);
+        getData.start();
+        getData.setType19(19, user_id);
+        getData.start();
+        getData.setType20(20, user_id);
+        getData.start();
+
+
+        postNum.setText(String.valueOf(getData.getPostNum()));
+        followerNum.setText(String.valueOf(getData.getFollowerNum()));
+        followingNum.setText(String.valueOf(getData.getFollowNum()));
 
 
         if(user_id == id){
