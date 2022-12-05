053a0a3d4c9c12233709082046315a3ace805579
“ssohye” <ssohye@icloud.com>
2022-12-05 19:02:33 +0900

    팔로우 취소 요청 기능 생성

--- src/function/clean_cache.java
+++ src/function/clean_cache.java
@@ -3,7 +3,31 @@ package function;
 import java.io.File;
 
 public class clean_cache {
+
+    public String makedir(){
+
+        String path = "post";
+        File Folder = new File(path);
+
+        // 해당 디렉토리가 없을경우 디렉토리를 생성합니다.
+        if (!Folder.exists()) {
+            try{
+                Folder.mkdir(); //폴더 생성합니다.
+                System.out.println("폴더가 생성되었습니다.");
+                return Folder.getAbsolutePath();
+            }
+            catch(Exception e){
+                e.getStackTrace();
+            }
+        }else {
+            System.out.println("이미 폴더가 생성되어 있습니다.");
+            return null;
+        }
+        return null;
+    }
     public clean_cache(String filepath) {
+
+        makedir();
 
         File path = new File(filepath);
         File[] fileList = path.listFiles();
